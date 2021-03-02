f678da4108aef9c6c92f04e5b477a7a812fda0a3
sandeepa dilshan <tosandeepa@gmail.com>
2021-03-03 02:47:08 +0900

    Sign In, Sign Out

--- App.js
+++ App.js
@@ -1,20 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
 import {
   Header,
@@ -24,91 +8,116 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+import Amplify ,{ Auth} from 'aws-amplify';
+import config from './src/aws-exports'
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignIn from './src/screens/SignIn';
+import SignUp from './src/screens/SignUp';
+import ConfirmSignUp from './src/screens/ConfirmSignUp';
+import Home from './src/screens/Home';
+Amplify.configure(config);
+
+import {withAuthenticator} from 'aws-amplify-react-native';
+
+const AuthenticationStack = createStackNavigator();
+const AppStack = createStackNavigator();
+const AuthenticationNavigator = props => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="SignIn">
+        {screenProps => (
+          <SignIn {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AuthenticationStack.Screen>
+      <AuthenticationStack.Screen name="SignUp" component={SignUp} />
+      <AuthenticationStack.Screen
+        name="ConfirmSignUp"
+        component={ConfirmSignUp}
+      />
+    </AuthenticationStack.Navigator>
   );
 };
 
+const AppNavigator = props => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Home">
+        {screenProps => (
+          <Home {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AppStack.Screen>
+    </AppStack.Navigator>
+  );
+};
+
+
+const Initializing = () => {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="tomato" />
+      </View>
+    );
+  };
+
+function App() {
+  const [isUserLoggedIn, setUserLoggedIn] = useState('initializing');
+
+  useEffect(() => {
+        checkAuthState();
+      }, []);
+      async function checkAuthState() {
+        try {
+          await Auth.currentAuthenticatedUser();
+          console.log(' User is signed in');
+          setUserLoggedIn('loggedIn');
+        } catch (err) {
+          console.log(' User is not signed in');
+          setUserLoggedIn('loggedOut');
+        }
+      }
+      function updateAuthState(isUserLoggedIn) {
+        setUserLoggedIn(isUserLoggedIn);
+      }
+
+      return (
+              <NavigationContainer>
+                {isUserLoggedIn === 'initializing' && <Initializing />}
+                {isUserLoggedIn === 'loggedIn' && (
+                  <AppNavigator updateAuthState={updateAuthState} />
+                )}
+                {isUserLoggedIn === 'loggedOut' && (
+                  <AuthenticationNavigator updateAuthState={updateAuthState} />
+                )}
+              </NavigationContainer>
+            );
+
+    // async function signOut() {
+    //   try {
+    //     await Auth.signOut();
+    //   } catch (error) {
+    //     console.log('Error signing out: ', error);
+    //   }
+    // }
+    // return (
+    //   <View style={styles.container}>
+    //     <Text> +  = React Native + Amplify </Text>
+    //     <Button title="Sign Out" color="tomato" onPress={signOut} />
+    //     <StatusBar style="auto" />
+    //   </View>
+    // );
+  }
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    marginTop: 250,
+    alignItems: 'center',
+    flex: 1
   },
 });
 
+// export default withAuthenticator(App);
 export default App;
+
